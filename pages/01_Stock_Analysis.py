3ac3fe6b205f0144588fe266af34417c367393da
João Cassis <joaopcassis@gmail.com>
2022-06-13 22:03:53 +0100

    first commit

---
+++ pages/01_Stock_Analysis.py
@@ -0,0 +1,186 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+import altair as alt
+import datetime
+
+st.set_page_config(layout='wide')
+
+START_DATE = '2010-01-01'
+
+
+@st.experimental_memo
+def get_ticker_data(ticker):
+    return yf.Ticker(ticker)
+
+
+@st.experimental_memo
+def get_drop_df(df, sp500):
+    maxes = df.groupby(pd.Grouper(freq='Y')).max()
+    while any(maxes.pct_change() < 0):
+        maxes = maxes[~(maxes.pct_change() < 0)]
+    years = maxes.index.year.unique()
+    drop_df = pd.DataFrame(columns=[
+        'start_idx',
+        'min_idx',
+        'recover_idx',
+        'end_idx',
+        ])
+
+    for i in range(len(years) - 1):
+        start_idx = df[str(years[i])].idxmax()
+        end_idx = df[str(years[i + 1])].idxmax()
+        min_idx = df[start_idx:end_idx].idxmin()
+        if df[start_idx: min_idx].max() > df[start_idx].max():
+            start_idx = df[start_idx:min_idx].idxmax()
+            if i != 0:
+                drop_df.loc[i - 1, 'end_idx'] = start_idx
+        recover_idx = df[min_idx:end_idx][
+                df[min_idx:end_idx] > df[start_idx]].index[0]
+        drop_df.loc[len(drop_df.index)] = [
+                start_idx,
+                min_idx,
+                recover_idx,
+                end_idx,
+                ]
+
+    max_idx = df.idxmax()
+    min_idx = drop_df.loc[len(drop_df.index)] = [
+            df.idxmax(),
+            df[max_idx:].idxmin(),
+            None,
+            df.index.max(),
+            ]
+
+    drop_df['price_start'] = df[drop_df.start_idx].reset_index(drop=True)
+    drop_df['price_min'] = df[drop_df.min_idx].reset_index(drop=True)
+    drop_df['price_end'] = df[drop_df.end_idx].reset_index(drop=True)
+    drop_df['pct_drop'] = drop_df[
+            ['price_start', 'price_min']].pct_change(
+                    axis='columns')['price_min']
+    drop_df['pct_gain'] = drop_df[
+            ['price_min', 'price_end']].pct_change(axis='columns')['price_end']
+    sp500_min = sp500[drop_df.min_idx].reset_index(drop=True)
+    sp500_max = sp500[drop_df.start_idx].reset_index(drop=True)
+    sp500_change = (sp500_min - sp500_max) / sp500_max
+    drop_df['sp500_change'] = sp500_change
+
+    drop_df['days_to_min'] = (
+            drop_df.min_idx - drop_df.start_idx).apply(lambda x: x.days)
+    drop_df['days_to_recover'] = (
+            drop_df.recover_idx - drop_df.min_idx).apply(lambda x: x.days).\
+        fillna(0).astype(int)
+    drop_df['days_to_new_peak_from_bottom'] = (
+            drop_df.end_idx - drop_df.min_idx).apply(lambda x: x.days)
+    drop_df['days_to_new_peak_from_previous'] = (
+            drop_df.end_idx - drop_df.start_idx).apply(lambda x: x.days)
+
+    assert (drop_df.days_to_min + drop_df.days_to_new_peak_from_bottom).equals(
+            drop_df.days_to_new_peak_from_previous)
+
+    drop_df = drop_df[drop_df.pct_drop < -0.1]
+    return drop_df
+
+
+ticker = st.text_input('TICKER')
+start_date = st.date_input(
+        'DATA INICIO',
+        value=datetime.datetime.strptime(START_DATE, '%Y-%m-%d'),
+        min_value=datetime.datetime.strptime('1993-01-29', '%Y-%m-%d'),
+        max_value=datetime.date.today(),
+        )
+if start_date < datetime.datetime.strptime('1993-01-29', '%Y-%m-%d').date():
+    st.warning('Data invalida. Dados a partir de 1993-1-29')
+    st.stop()
+
+data = get_ticker_data(ticker)
+df = data.history(start=start_date)['Close']
+if df.empty:
+    st.warning('Escolha um TICKER válido')
+    st.stop()
+
+sp500 = get_ticker_data('SPY').history(
+        start=start_date).loc[df.index.min():, 'Close']
+
+drop_df = get_drop_df(df, sp500)
+
+layers = alt.layer()
+layers += alt.Chart(df.reset_index()).mark_line().encode(
+        x="Date:T",
+        y="Close:Q",
+        )
+for _, row in drop_df.iterrows():
+    layers += alt.Chart(
+            df[row.start_idx: row.min_idx].reset_index()
+            ).mark_line(color='orange').encode(
+        x="Date:T",
+        y="Close:Q",
+        tooltip=alt.value(f'{row.pct_drop:.0%}')
+        )
+
+st.title(f'{ticker}')
+with st.expander('Explicação (clique para abrir)'):
+    st.markdown('''
+     - No grafico
+        - A **LARANJA** as quedas superiores a 10%
+        - Apontar com o rato para o tracejado laranja para saber o valor
+        - O grafico e interactivo (mexer e ampliar com o rato)
+            - Duplo-clique para voltar ao inicio
+     - Primeira tabela
+        - Cada linha corresponde a uma quebra assinalada no grafico
+     - Segunda tabela
+        - Minimo, Maximo e Media dos valores
+        ''')
+st.altair_chart(layers.interactive(), use_container_width=True)
+
+st.dataframe(drop_df)
+
+st.write(drop_df[:-1].describe().loc[
+    ['min', 'max', 'mean'],
+    ['pct_drop', 'pct_gain', 'sp500_change',
+        'days_to_min', 'days_to_recover',
+        'days_to_new_peak_from_bottom',
+        'days_to_new_peak_from_previous', ]])
+
+
+sp500_pct = sp500 / sp500.iat[0] - 1
+sp500_pct = pd.concat(
+        [sp500_pct, pd.Series('SP500', index=sp500_pct.index)],
+        axis=1,
+        keys=['Close', 'Ticker'],
+        )
+ticker_pct = df / df.iat[0] - 1
+ticker_pct = pd.concat(
+        [ticker_pct, pd.Series(f'{ticker}', index=df.index)],
+        axis=1,
+        keys=['Close', 'Ticker'],
+        )
+combined_pct = pd.concat([
+    sp500_pct, ticker_pct], axis=0)
+
+nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                        fields=['Date'], empty='none')
+selectors = alt.Chart(combined_pct.reset_index()).mark_point().encode(
+        x='Date:T',
+        opacity=alt.value(0),
+        ).add_selection(nearest)
+
+line = alt.Chart(combined_pct.reset_index()).mark_line().encode(
+        x='Date:T',
+        y=alt.Y('Close:Q', title='Percentage Change'),
+        color='Ticker',
+        )
+
+points = line.mark_point().encode(
+    opacity=alt.condition(nearest, alt.value(1), alt.value(0)))
+
+rules = alt.Chart(combined_pct.reset_index()).mark_rule(color='gray').encode(
+    x='Date:T',
+    ).transform_filter(nearest)
+
+text = line.mark_text(align='left', dx=5, dy=-5).encode(
+    text=alt.condition(nearest, 'Close:Q', alt.value(' '))
+    )
+layer = alt.layer(line, selectors, points, rules, text).interactive()
+st.title(f'{ticker} vs SP500')
+st.altair_chart(layer, use_container_width=True)
---
+++ pages/02_Tesla_Sentiment.py
@@ -0,0 +1,54 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+import altair as alt
+
+st.set_page_config(layout='wide')
+
+START_DATE = '2010-01-01'
+
+
+@st.experimental_singleton
+def init_engine():
+    return create_engine(
+        f'postgresql://'
+        f'{st.secrets["postgres"]["user"]}:'
+        f'{st.secrets["postgres"]["password"]}@'
+        f'{st.secrets["postgres"]["host"]}:'
+        f'{st.secrets["postgres"]["port"]}/'
+        f'{st.secrets["postgres"]["dbname"]}',
+        )
+
+
+@st.experimental_memo
+def get_ticker_data(ticker):
+    return yf.Ticker(ticker)
+
+
+engine = init_engine()
+
+sentiment_df = pd.read_sql('''
+SELECT date, positive, negative, neutral FROM tesla_news''', engine)
+sentiment_df['sentiment'] = sentiment_df.positive - sentiment_df.negative * 5
+sentiment_df = sentiment_df.groupby('date').agg({'sentiment': 'sum'})
+sentiment_df.sentiment = sentiment_df.sentiment.cumsum()
+
+data = get_ticker_data('TSLA')
+df = data.history(start=START_DATE)['Close']
+
+stock = alt.Chart(df.reset_index()).mark_line().encode(
+        x="Date:T",
+        y="Close:Q",
+        )
+
+sentiment = alt.Chart(
+        sentiment_df.reset_index()
+        ).mark_line(color='orange').encode(
+            x='date:T',
+            y='sentiment:Q',
+            )
+
+layer = alt.layer(stock, sentiment).resolve_scale(y='independent')
+
+st.altair_chart(layer, use_container_width=True)
