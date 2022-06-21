c55edf812041be5314c4fc7c48939675ec0411ea
João Cassis <joaopcassis@gmail.com>
2022-06-21 21:18:22 +0100

    Added heroku VARS in TESLA SENTIMENT

--- pages/02_Tesla_Sentiment.py
+++ pages/02_Tesla_Sentiment.py
@@ -1,23 +1,33 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
-from sqlalchemy import create_engine
 import altair as alt
+from sqlalchemy import create_engine
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout="wide")
 
-START_DATE = '2010-01-01'
+START_DATE = "2010-01-01"
 
 
 @st.experimental_singleton
 def init_engine():
-    return create_engine(
-        f'postgresql://'
-        f'{st.secrets["postgres"]["user"]}:'
-        f'{st.secrets["postgres"]["password"]}@'
-        f'{st.secrets["postgres"]["host"]}:'
-        f'{st.secrets["postgres"]["port"]}/'
-        f'{st.secrets["postgres"]["dbname"]}',
+    try:
+        create_engine(
+            f"postgresql://"
+            f'{st.secrets["postgres"]["user"]}:'
+            f'{st.secrets["postgres"]["password"]}@'
+            f'{st.secrets["postgres"]["host"]}:'
+            f'{st.secrets["postgres"]["port"]}/'
+            f'{st.secrets["postgres"]["dbname"]}',
+        )
+    except Exception:
+        DB_USER = os.environ.get("DB_USER")
+        DB_PSWD = os.environ.get("DB_PSWD")
+        DB_HOST = os.environ.get("DB_HOST")
+        DB_PORT = os.environ.get("DB_PORT")
+        DB_NAME = os.environ.get("DB_NAME")
+        create_engine(
+            f"postgresql://" f"{DB_USER}:{DB_PSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
         )
 
 
@@ -27,28 +37,36 @@ def get_ticker_data(ticker):
 
 
 engine = init_engine()
-
-sentiment_df = pd.read_sql('''
-SELECT date, positive, negative, neutral FROM tesla_news''', engine)
-sentiment_df['sentiment'] = sentiment_df.positive - sentiment_df.negative * 5
-sentiment_df = sentiment_df.groupby('date').agg({'sentiment': 'sum'})
+sentiment_df = pd.read_sql(
+    """
+SELECT date, positive, negative, neutral FROM tesla_news""",
+    engine,
+)
+sentiment_df["sentiment"] = sentiment_df.positive - sentiment_df.negative * 5
+sentiment_df = sentiment_df.groupby("date").agg({"sentiment": "sum"})
 sentiment_df.sentiment = sentiment_df.sentiment.cumsum()
 
-data = get_ticker_data('TSLA')
-df = data.history(start=START_DATE)['Close']
+data = get_ticker_data("TSLA")
+df = data.history(start=START_DATE)["Close"]
 
-stock = alt.Chart(df.reset_index()).mark_line().encode(
+stock = (
+    alt.Chart(df.reset_index())
+    .mark_line()
+    .encode(
         x="Date:T",
         y="Close:Q",
-        )
+    )
+)
 
-sentiment = alt.Chart(
-        sentiment_df.reset_index()
-        ).mark_line(color='orange').encode(
-            x='date:T',
-            y='sentiment:Q',
-            )
+sentiment = (
+    alt.Chart(sentiment_df.reset_index())
+    .mark_line(color="orange")
+    .encode(
+        x="date:T",
+        y="sentiment:Q",
+    )
+)
 
-layer = alt.layer(stock, sentiment).resolve_scale(y='independent')
+layer = alt.layer(stock, sentiment).resolve_scale(y="independent")
 
 st.altair_chart(layer, use_container_width=True)
--- pages/03_Food.py
+++ pages/03_Food.py
@@ -3,38 +3,37 @@ from streamlit_folium import st_folium
 import folium
 from PIL import Image
 from io import BytesIO
-import psycopg2
 from folium.plugins import MarkerCluster
 import pandas as pd
+from sqlalchemy import create_engine
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout="wide")
 proximity_round = 2
 
 
 @st.experimental_singleton
-def init_connection():
-    connection = psycopg2.connect(**st.secrets['postgres'])
-    connection.autocommit = True
-    return connection
+def init_engine():
+    return create_engine(
+        f"postgresql://"
+        f'{st.secrets["postgres"]["user"]}:'
+        f'{st.secrets["postgres"]["password"]}@'
+        f'{st.secrets["postgres"]["host"]}:'
+        f'{st.secrets["postgres"]["port"]}/'
+        f'{st.secrets["postgres"]["dbname"]}',
+    )
 
 
-connection = init_connection()
-
-
-def run_query(connection, query, fetch=None):
-    with connection.cursor() as cur:
-        cur.execute(query)
-        connection.commit()
-        if fetch:
-            return cur.fetchall()
+engine = init_engine()
 
 
 @st.experimental_memo(ttl=6000)
 def get_db_data():
-    data = run_query(connection, """
-            SELECT * from fotos
-            WHERE json->>'latitude' is NOT NULL""", 1)
-    return data
+    return pd.read_sql(
+        """
+    SELECT * from fotos
+    WHERE json->>'latitude' is NOT NULL""",
+        engine,
+    )
 
 
 def get_image_from_byte_array(byte_array):
@@ -44,49 +43,57 @@ def get_image_from_byte_array(byte_array):
 
 
 def main():
-    st.header("""
+    st.header(
+        """
     Vegan Food 🌱 around the World 🗺️
-    """)
+    """
+    )
     data = get_db_data()
-    m = folium.Map(
-            location=[38.699638, -9.267694],
-            zoom_start=3, tiles='cartodbpositron')
-    fg = folium.FeatureGroup(name='Marks')
     df = pd.DataFrame()
-    for i in data:
-        metadata = i[1]
-        latitude = round(metadata['latitude'], proximity_round)
-        longitude = round(metadata['longitude'], proximity_round)
-        country = metadata['place']['address']['country']
-        df = pd.concat([df, pd.DataFrame.from_records([{
-            'country': country,
-            'latitude': latitude,
-            'longitude': longitude}])])
+    for i in data.json:
+        latitude = round(i["latitude"], proximity_round)
+        longitude = round(i["longitude"], proximity_round)
+        country = i["place"]["address"]["country"]
+        df = pd.concat(
+            [
+                df,
+                pd.DataFrame.from_records(
+                    [{"country": country, "latitude": latitude, "longitude": longitude}]
+                ),
+            ]
+        )
     df = df.drop_duplicates()
-    fg = folium.FeatureGroup(name='Marks')
-    for _, country_df in df.groupby('country'):
-        MarkerCluster(country_df[['latitude', 'longitude']].values).add_to(fg)
+
+    m = folium.Map(
+        location=[38.699638, -9.267694], zoom_start=3, tiles="cartodbpositron"
+    )
+    fg = folium.FeatureGroup(name="Marks")
+    for _, country_df in df.groupby("country"):
+        MarkerCluster(country_df[["latitude", "longitude"]].values).add_to(fg)
     m.add_child(fg)
-    folium_data = st_folium(m, width=725, key='map')
+    folium_data = st_folium(m, width=725, key="map")
+
     cols = st.columns(3)
     try:
-        clicked_lat = folium_data['last_object_clicked']['lat']
-        clicked_lng = folium_data['last_object_clicked']['lng']
+        clicked_lat = folium_data["last_object_clicked"]["lat"]
+        clicked_lng = folium_data["last_object_clicked"]["lng"]
         results = [
-                i[0] for i in data if (
-                    round(i[1]['latitude'], proximity_round) == clicked_lat and
-                    round(i[1]['longitude'], proximity_round) == clicked_lng)]
+            row.id
+            for _, row in data.iterrows()
+            if round(row.json["latitude"], proximity_round) == clicked_lat
+            and round(row.json["longitude"], proximity_round) == clicked_lng
+        ]
         i = 0
-        url_prefix = 'https://res.cloudinary.com/kassiusklay/'
+        url_prefix = "https://res.cloudinary.com/kassiusklay/"
         for result in results:
-            cols[i].image(f'{url_prefix + result}')
+            cols[i].image(f"{url_prefix + result}")
             i += 1
             if i == 3:
                 i = 0
                 cols = st.columns(3)
     except TypeError:
-        st.warning('Por favor clique num ponto para ver a imagem')
+        st.warning("Por favor clique num ponto para ver a imagem")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
